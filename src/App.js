0db287611a5ca68b6e54fb9fb60c7e54f3d95451
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-07 17:15:04 +0200

    Add fixed action btn to add logs and techs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-
 import './App.css';
 
 const App = () => {
-  useEffect(() => {
-    // Init Materialize JS
-    M.AutoInit();
-  })
-  return (
-    <Fragment>
-      <SearchBar />
-      <div className="container"><Logs /></div>
-    </Fragment>
-  );
-}
+    useEffect(() => {
+        // Init Materialize JS
+        M.AutoInit();
+    });
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <Logs />
+            </div>
+        </Fragment>
+    );
+};
 
 export default App;
