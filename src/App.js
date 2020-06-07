e8d726b612e8117b65d3e28c1eab1cb65969bc1f
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-07 14:25:58 +0200

    Add LogItem and preloader component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
-import Logs from './components/logs/LogItem';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
