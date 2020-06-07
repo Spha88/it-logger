537f9008b9063fe7b4e209fc9957d161b512593c
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-07 08:41:42 +0200

    Add Logs Componenent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/LogItem';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -15,6 +16,7 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
+      <div className="container"><Logs /></div>
     </Fragment>
   );
 }
